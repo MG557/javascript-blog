35922fce5643f3aebed1479982424c882b8aba5d
Marcin Grajdek <grajdek.m@vp.pl>
2022-02-19 13:34:12 +0100

    Add correct - zd. 6.4

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@
     event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
-  
+
     /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -23,9 +23,9 @@
 
     clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);
-    
+
     /* [DONE] remove class 'active' from all articles */
- 
+
     const activeArticles = document.querySelectorAll('.posts article.active');
     //  .titles a.active
     for(let activeArticle of activeArticles){
@@ -33,7 +33,7 @@
     }
     /* [DONE] get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
-    
+
     console.log('click articleSelector:', articleSelector);
 
     /* [DONE] find the correct article using the selector (value of 'href' attribute) */
@@ -42,8 +42,8 @@
 
     /* [DONE] add class 'active' to the correct article */
     //document.querySelector('href').classList.add('active');
-   
-    targetArticle.classList.add('active'); 
+
+    targetArticle.classList.add('active');
   // eslint-disable-next-line semi
   }
 
@@ -58,24 +58,24 @@
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles';
- 
+
   // eslint-disable-next-line no-inner-declarations
   function generateTitleLinks(){
     console.log('function generateTitleLinks!');
-   
+
     /* remove contents of titleList */
     const titleList = document.querySelector(optTitleListSelector);
     titleList.innerHTML = '';
     //clearMessages();
-    
+
     /* for each article */
     const articles = document.querySelectorAll(optArticleSelector);
     let html = '';
     for(let article of articles){
-      article.classList.remove('active');
+      article.classList.remove('.titles');
       //let html = '';
       //console.log('html', html);
-      
+
       /* get the article id */
 
       const articleId = article.getAttribute('id');
@@ -85,25 +85,25 @@
 
       const articleTitle = article.querySelector(optTitleSelector).innerHTML;
       console.log('articleTitle', articleTitle);
-     
+
       /* get the title from the title element */
-      
-      article.classList.add('active');
-      
+
+      article.classList.add('.post');
+
       /* create HTML of the link */
       const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-     
+
       /* insert link into titleList */
-     
+
       //titleList.innerHTML = titleList.innerHTML + linkHTML;
       //console.log('titlelist', titleList);
       titleList.insertAdjacentHTML('beforeend', linkHTML);
-      
+
       html = html + linkHTML;
-      
+
     }
     //console.log('html', html);
-      
+
     titleList.innerHTML = html;
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
@@ -111,6 +111,6 @@
       link.addEventListener('click', titleClickHandler);
     }
   }
- 
+
   generateTitleLinks();
 }
